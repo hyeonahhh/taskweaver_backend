16b76bcc066a0113f44ee04604f7f46c0ae642ad
bboggo <kmt0819@naver.com>
2024-01-28 03:22:45 +0900

    #7 feat: add TeamServiceImpl.java

--- src/main/java/backend/taskweaver/domain/team/service/TeamServiceImpl.java
+++ src/main/java/backend/taskweaver/domain/team/service/TeamServiceImpl.java
@@ -1,20 +1,23 @@
 package backend.taskweaver.domain.team.service;
 
-
 import backend.taskweaver.domain.team.dto.TeamRequest;
 import backend.taskweaver.domain.team.dto.TeamResponse;
 import backend.taskweaver.domain.team.entity.Team;
 import backend.taskweaver.domain.team.repository.TeamRepository;
-
 import backend.taskweaver.global.converter.TeamConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
-public interface TeamService {
-
+@RequiredArgsConstructor
+@Service
+public class TeamServiceImpl implements TeamService{
+    @Autowired
+    private final TeamRepository teamRepository;
     // 우선 팀 생성자 필드로만 추가
-    public TeamResponse.teamCreateResult createTeam(TeamRequest.teamCreate request, Long user);
+    public TeamResponse.teamCreateResult createTeam(TeamRequest.teamCreate request, Long user) {
+        Team team =  teamRepository.save(TeamConverter.toTeam(request));
+        team.setTeamLeader(user);
+        return TeamConverter.toCreateResponse(team);
+    }
 }
