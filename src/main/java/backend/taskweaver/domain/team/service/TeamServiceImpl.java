177eb332b9cbb28c978cdbed4157b4a17d400732
bboggo <kmt0819@naver.com>
2024-01-31 01:54:30 +0900

    #12 chore: 해당 유저 정보 가져오기

--- src/main/java/backend/taskweaver/domain/team/service/TeamServiceImpl.java
+++ src/main/java/backend/taskweaver/domain/team/service/TeamServiceImpl.java
@@ -1,15 +1,24 @@
 package backend.taskweaver.domain.team.service;
 
+import backend.taskweaver.domain.member.entity.Member;
 import backend.taskweaver.domain.member.repository.MemberRepository;
 import backend.taskweaver.domain.team.dto.TeamInviteRequest;
 import backend.taskweaver.domain.team.dto.TeamRequest;
 import backend.taskweaver.domain.team.dto.TeamResponse;
 import backend.taskweaver.domain.team.entity.Team;
 import backend.taskweaver.domain.team.repository.TeamRepository;
+import backend.taskweaver.global.code.ErrorCode;
 import backend.taskweaver.global.converter.TeamConverter;
+import backend.taskweaver.global.exception.handler.BusinessExceptionHandler;
+import backend.taskweaver.global.exception.handler.GlobalExceptionHandler;
+import ch.qos.logback.core.spi.ErrorCodes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +28,7 @@ public class TeamServiceImpl implements TeamService{
     @Autowired
     private final MemberRepository memberRepository;
 
+    // 팀 생성
     // 우선 팀 생성자 필드로만 추가
     public TeamResponse.teamCreateResult createTeam(TeamRequest.teamCreateRequest request, Long user) {
         Team team =  teamRepository.save(TeamConverter.toTeam(request));
@@ -26,7 +36,20 @@ public class TeamServiceImpl implements TeamService{
         return TeamConverter.toCreateResponse(team);
     }
 
+    // 팀 초대
     public TeamInviteRequest.EmailInviteRequest inviteEmail(TeamInviteRequest.EmailInviteRequest request) {
+        List<Member> members = memberRepository.findAll();
+        Optional<Member> matchingMember = members.stream()
+                .filter(member -> member.getEmail().equals(request.getEmail()))
+                .findFirst();
+
+        if (matchingMember.isPresent()) {
+            Long userId = matchingMember.get().getId();
+            System.out.println(userId);
+        } else {
+            // 처리 필요
+            throw new BusinessExceptionHandler(ErrorCode.NOT_EXIST_MEMBER);
+        }
         return request;
     }
 }
