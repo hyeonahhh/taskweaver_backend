054e91bca46bc55908451d387b009f6085252ba3
YounCheEun <amy4021@naver.com>
2024-01-30 22:08:50 +0900

    feat: add token exceptions

--- src/main/java/backend/taskweaver/global/security/JwtAuthenticationFilter.java
+++ src/main/java/backend/taskweaver/global/security/JwtAuthenticationFilter.java
@@ -1,15 +1,19 @@
 package backend.taskweaver.global.security;
 
+import backend.taskweaver.global.code.ErrorCode;
+import backend.taskweaver.global.exception.handler.JwtTokenHandler;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -24,6 +28,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Order(0)
 @Component
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
@@ -37,12 +42,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             AbstractAuthenticationToken authenticated = UsernamePasswordAuthenticationToken.authenticated(user, accessToken, user.getAuthorities());
             authenticated.setDetails(new WebAuthenticationDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticated);
-        } catch (ExpiredJwtException e) {	// 변경
-            e.printStackTrace();
+        } catch (ExpiredJwtException e) {	// 토큰이 만료됐을 때
+            log.error("the token is expired and not valid anymore", e);
             reissueAccessToken(request, response, e);
-        } catch (Exception e) {
-            e.printStackTrace();
-            request.setAttribute("exception", e);
+            throw new JwtTokenHandler(ErrorCode.EXPIRED_JWT_ERROR);
+        } catch (IllegalArgumentException e) {
+            log.error("an error occured during getting username from token", e);
+            throw new JwtTokenHandler(ErrorCode.INVALID_JWT_ERROR);
+        } catch (AuthenticationException e){
+            log.error("Authentication Failed. Username or Password not valid.", e);
+            throw new JwtTokenHandler(ErrorCode.USER_AUTH_ERROR);
         }
 
         filterChain.doFilter(request, response);
