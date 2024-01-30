20bde5f7d4c1156ecfd9c02c7cfb6d6f9f780e97
YounCheEun <amy4021@naver.com>
2024-01-31 07:04:18 +0900

    fix: fix project controller response

--- src/main/java/backend/taskweaver/domain/project/controller/ProjectController.java
+++ src/main/java/backend/taskweaver/domain/project/controller/ProjectController.java
@@ -1,8 +1,9 @@
 package backend.taskweaver.domain.project.controller;
 
 import backend.taskweaver.domain.project.dto.ProjectRequest;
-import backend.taskweaver.domain.project.dto.ProjectResponse;
 import backend.taskweaver.domain.project.service.ProjectServiceImpl;
+import backend.taskweaver.global.code.ApiResponse;
+import backend.taskweaver.global.code.SuccessCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +18,14 @@ public class ProjectController {
     private final ProjectServiceImpl projectService;
 
     @PostMapping("/team/{teamId}/project")
-    public ResponseEntity<ProjectResponse> addProject(@RequestBody @Valid ProjectRequest request,
-                                                      @PathVariable Long teamId) {
-        ProjectResponse response = projectService.createProject(request, teamId);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(response);
+    public ResponseEntity<ApiResponse> addProject(@RequestBody @Valid ProjectRequest request,
+                                                  @PathVariable Long teamId) {
+        ApiResponse apiResponse = ApiResponse.builder()
+                .result(projectService.createProject(request, teamId))
+                .resultCode(SuccessCode.INSERT_SUCCESS.getStatus())
+                .resultMsg(SuccessCode.INSERT_SUCCESS.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(apiResponse);
     }
-
 }
