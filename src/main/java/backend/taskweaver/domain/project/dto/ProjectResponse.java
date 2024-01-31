58259cd1fc53932b3e901f3aee4a4e1331e45332
YounCheEun <amy4021@naver.com>
2024-01-31 10:26:48 +0900

    feat: add swagger code

--- src/main/java/backend/taskweaver/domain/project/dto/ProjectResponse.java
+++ src/main/java/backend/taskweaver/domain/project/dto/ProjectResponse.java
@@ -1,12 +1,23 @@
 package backend.taskweaver.domain.project.dto;
 
 import backend.taskweaver.domain.project.entity.enums.ProjectStateName;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "프로젝트 응답 DTO")
 public record ProjectResponse(
-     Long projectId,
-     String name,
-     String description,
-     Long managerId,
-     ProjectStateName projectState
+        @Schema(description = "프로젝트 ID", example = "1")
+        Long projectId,
+
+        @Schema(description = "프로젝트 제목", example = "oo과목 중간 대체 프로젝트")
+        String name,
+
+        @Schema(description = "프로젝트 설명", example = "이 프로젝트는 oo과목을 위한 프로젝트입니다.")
+        String description,
+
+        @Schema(description = "프로젝트 담당자 ID", example = "1")
+        Long managerId,
+
+        @Schema(description = "프로젝트 상태", example = "ON_PROGRESS")
+        ProjectStateName projectState
 ) {
 }
