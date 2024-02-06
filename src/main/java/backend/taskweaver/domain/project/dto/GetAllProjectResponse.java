4fe95b3a4851300c403891fdcb592fe482481865
YounCheEun <amy4021@naver.com>
2024-02-06 16:19:16 +0900

    feat: add swagger code

--- src/main/java/backend/taskweaver/domain/project/dto/GetAllProjectResponse.java
+++ src/main/java/backend/taskweaver/domain/project/dto/GetAllProjectResponse.java
@@ -1,7 +1,13 @@
 package backend.taskweaver.domain.project.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "프로젝트 전체 조회 응답 DTO")
 public record GetAllProjectResponse(
-      Long projectId,
-      String projectName
+        @Schema(description = "프로젝트 ID", example = "1")
+        Long projectId,
+
+        @Schema(description = "프로젝트 제목", example = "oo과목 중간 대체 프로젝트")
+        String projectName
 ) {
 }
