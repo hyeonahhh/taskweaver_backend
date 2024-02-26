73e940a2e88998181e1c0dba295761021c8c6414
YounChaeEun <amy4021186@gmail.com>
2024-02-26 13:55:52 +0900

    fix: successCode에서 updateSuccess code 200으로 수정

--- src/main/java/backend/taskweaver/global/code/SuccessCode.java
+++ src/main/java/backend/taskweaver/global/code/SuccessCode.java
@@ -15,7 +15,7 @@ public enum SuccessCode {
     // 삽입 성공 코드 (HTTP Response: 201 Created)
     INSERT_SUCCESS(201, "201", "INSERT SUCCESS"),
     // 수정 성공 코드 (HTTP Response: 201 Created)
-    UPDATE_SUCCESS(200, "204", "UPDATE SUCCESS"),
+    UPDATE_SUCCESS(200, "200", "UPDATE SUCCESS"),
 
     ; // End
 
