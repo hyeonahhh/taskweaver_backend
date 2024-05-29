cb78c2c6b6be2b7f90a148220baf2c0cf8ede241
bboggo <kmt0819@naver.com>
2024-05-30 01:11:57 +0900

    #79 feat: 알림 조회시 member 테이블의 isRead YES로 변경

--- src/main/java/backend/taskweaver/domain/notification/service/NotificationServiceImpl.java
+++ src/main/java/backend/taskweaver/domain/notification/service/NotificationServiceImpl.java
@@ -1,8 +1,10 @@
 package backend.taskweaver.domain.notification.service;
 
+import backend.taskweaver.domain.member.repository.MemberRepository;
 import backend.taskweaver.domain.notification.dto.NotificationResponse;
 import backend.taskweaver.domain.notification.entity.Notification;
 import backend.taskweaver.domain.notification.entity.NotificationMember;
+import backend.taskweaver.domain.notification.entity.enums.isRead;
 import backend.taskweaver.domain.notification.repository.NotificationMemberRepository;
 import backend.taskweaver.domain.notification.repository.NotificationRepository;
 import backend.taskweaver.global.converter.NotificationConverter;
@@ -22,9 +24,16 @@ public class NotificationServiceImpl implements NotificationService{
 
     private final NotificationMemberRepository notificationMemberRepository;
     private final NotificationRepository notificationRepository;
+    private final MemberRepository memberRepository; // MemberRepository 추가
     public List<NotificationResponse.AllNotificationInfo> getAllNotificationsForUser(Long memberId) {
         // 로그인한 사용자의 모든 알림 조회
         List<NotificationMember> notificationMembers = notificationMemberRepository.findByMemberId(memberId);
+
+        // Member의 isRead 값을 'YES'로 업데이트
+        memberRepository.findById(memberId).ifPresent(member -> {
+            member.setIsRead(isRead.YES); // setIsRead 메소드를 Member 엔티티에 추가해야 함
+            memberRepository.save(member);
+        });
 
         // 조회된 알림을 NotificationResponse.AllNotificationInfo 객체로 변환
         return notificationMembers.stream()
